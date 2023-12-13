8b924d9b2df738dd815f3d04d32a2d13ad70e5f2
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2023-12-13 16:45:17 +0100

    Faster create_pseudobulks function.
    Faster create_pseudobulks function, by assuming that a lot of consecutive
    values will have the same value. Also streches of zeros are not stored at all.
    
    Based on:
      https://github.com/aertslab/single_cell_toolkit/blob/c15038ddf1322fd4957396c16bb7782ad2e6629e/fragments_to_bw.py#L263C1-L372C82

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
         "pandas >= 1.3.3",
         "pyBigWig >= 0.3.17",
         "torch >= 1.9.0",
-        "tables >= 3.8.0"
+        "tables >= 3.8.0",
+        "numba >= 0.55"
     ],
 )
